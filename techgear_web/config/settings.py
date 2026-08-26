392c325e3fdf621d2e313567c8f3184acf2d208c
LuisOrCo <angelortegacorrea09@gmail.com>
2026-08-26 14:54:07 -0500

    chore: Actualizar los requirements.txt

--- techgear_web/config/settings.py
+++ techgear_web/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dgjva@h388gpbd%d#q%f81i7rf*#!d%$5bby1)6ed)p1sq27q&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.vercel.app',
+    '.onrender.com',
+]
 
 
 # Application definition
