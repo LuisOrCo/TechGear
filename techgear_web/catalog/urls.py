78b085cb4941fc6975b9202ba3e1353e5d4210e4
LuisOrCo <angelortegacorrea09@gmail.com>
2026-08-30 20:28:31 -0500

    fix: Reparar la ruta del punto de entrada de la aplicación.

--- techgear_web/catalog/urls.py
+++ techgear_web/catalog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import product_list, checkout
 
 urlpatterns = [
-    path("products/", product_list, name="product_list"),
+    path("", product_list, name="product_list"),
+    path("products/", product_list),
     path("checkout/<str:product_id>/", checkout, name="checkout"),
     path("checkout/", checkout, name="checkout"),
 ]
